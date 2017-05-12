e66b931db6f2c51be90e4a4097f650f02bdf614d
Jklars <pokeofdoom77@gmail.com>
2017-05-12 21:54:28 +0300

    Create backup.sh
    Adds backup option

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Starting Backup
+echo Detecting Data Partition Position
+sudo fdisk -l | grep msftdata >> msftdata
+
+python3 givepos.py
+
+export MSFTDATA=(<msftdata)
+
+echo Mounting Data Partition
+mkdir /mnt/msftdata && sudo mount /dev/sda$MSFTDATA /mnt
+
+mkdir ~/msbackup
+
+echo Copying Files
+cp -v /mnt/msftdata/* ~/msbackup
+
+echo Unmounting Data Partition
+sudo umount /dev/sda$MSFTDATA
